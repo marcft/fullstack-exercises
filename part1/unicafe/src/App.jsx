ee00f73afeecfbf653324035f8dc6782c867b22b
Marc Frances <marc.frances.t@gmail.com>
2024-03-28 20:29:13 +0100

    Activity 1.11 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,9 +3,10 @@ import { useState } from 'react'
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const StatisticLine = ({ text, value }) => (
-  <p>
-    {text} {value}
-  </p>
+  <tr>
+    <th scope="row">{text}</th>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -16,18 +17,17 @@ const Statistics = ({ good, neutral, bad }) => {
   }
 
   return (
-    <>
-      <div>
+    <table>
+      <tbody>
         <StatisticLine text="good" value={good} />
         <StatisticLine text="neutral" value={neutral} />
         <StatisticLine text="bad" value={bad} />
-      </div>
-      <div>
+
         <StatisticLine text="all" value={total} />
         <StatisticLine text="average" value={(good - bad) / total} />
         <StatisticLine text="positive" value={`${(good / total) * 100} %`} />
-      </div>
-    </>
+      </tbody>
+    </table>
   )
 }
 
