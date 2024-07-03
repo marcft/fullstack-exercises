272f66598647a5aff67d17bc8ac7052a28a87d02
Marc Frances <marc.frances.t@gmail.com>
2024-07-03 13:14:23 +0200

    Activity 5.7 done

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <li>
-    {blog.title}, {blog.author}
-  </li>
-)
+import Togglable from './Togglable'
+
+const TogglableWithTitle = (props) => {
+  return (
+    <div>
+      {props.title} <Togglable buttonLabel="view">{props.children}</Togglable>
+    </div>
+  )
+}
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <li style={blogStyle}>
+      <TogglableWithTitle title={blog.title}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.author}</div>
+      </TogglableWithTitle>
+    </li>
+  )
+}
 
 export default Blog
