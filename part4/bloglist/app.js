e14346a861eb2ce5eeabab05364cc1b58e8bb395
Marc Frances <marc.frances.t@gmail.com>
2024-07-08 12:41:41 +0200

    Add a testing router to clear db

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const { MONGODB_URI } = require('./utils/config')
 const loginRouter = require('./controllers/login')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const testingRouter = require('./controllers/testing')
 const {
   morganLogger,
   tokenExtractor,
@@ -35,6 +36,10 @@ app.use(tokenExtractor)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
