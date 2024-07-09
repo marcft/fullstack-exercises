c1a857fa59d712597ecb4ffb3872aeeceee4233a
Marc Frances <marc.frances.t@gmail.com>
2024-07-09 18:42:30 +0200

    Activity 6.2 done

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -5,6 +5,7 @@ import reducer from './reducer'
 
 const store = createStore(reducer)
 
+// eslint-disable-next-line react-refresh/only-export-components
 const App = () => {
   const good = () => {
     store.dispatch({
@@ -12,15 +13,33 @@ const App = () => {
     })
   }
 
+  const ok = () => {
+    store.dispatch({
+      type: 'OK',
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD',
+    })
+  }
+
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO',
+    })
+  }
+
   return (
     <div>
       <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={ok}>ok</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={zero}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
