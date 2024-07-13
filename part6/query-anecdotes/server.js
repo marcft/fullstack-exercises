0fa49ed439bf3d9e2206668e7918cd8e6ce37462
Marc Frances <marc.frances.t@gmail.com>
2024-07-13 10:40:50 +0200

    Activity 6.21 - 6.22 done

--- part6/query-anecdotes/server.js
+++ part6/query-anecdotes/server.js
@@ -5,8 +5,6 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 const validator = (request, response, next) => {
-  console.log()
-
   const { content } = request.body
 
   if (request.method === 'POST' && (!content || content.length < 5)) {
--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,20 +1,36 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { getAnecdotes } from './requests'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      // queryClient.invalidateQueries({ queryKey: ['anecdotes'] }) Triggers GET again
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(
+        ['anecdotes'],
+        anecdotes.map((anecdote) =>
+          anecdote.id == updatedAnecdote.id ? updatedAnecdote : anecdote
+        )
+      )
+    },
+  })
+
+  const handleVote = (anecdote) => {
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+  }
+
   const result = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
     refetchOnWindowFocus: false,
     retry: 1,
   })
-
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
 
   if (result.isPending) {
     return <div>loading anecdotes...</div>
