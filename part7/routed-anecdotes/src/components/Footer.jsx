399bebe6e51b4bc28fcb08e1da78c77eaf93983f
Marc Frances <marc.frances.t@gmail.com>
2024-07-17 09:34:50 +0200

    Activity 7.2 done

---
+++ part7/routed-anecdotes/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => (
+  <div>
+    Anecdote app for <a href="https://fullstackopen.com/">Full Stack Open</a>.
+    Follow{' '}
+    <a href="https://github.com/marcft/fullstack-exercises/blob/main/part7/routed-anecdotes/src/App.jsx">
+      this link
+    </a>{' '}
+    for the source code.
+  </div>
+)
+
+export default Footer
